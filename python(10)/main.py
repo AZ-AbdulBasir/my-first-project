f8067fca84477c46ec49bcaece5fee3c5e9c0f64
Abdul-Basir Azamat-Aliev <97284448+AZ-AbdulBasir@users.noreply.github.com>
2022-01-09 13:41:16 +0500

    Add files via upload

---
+++ python(10)/main.py
@@ -0,0 +1,67 @@
+my_list = [4, 6, 2, 8, 3, 1]
+my_list.append(9)
+print(my_list)
+tuple(my_list)
+
+
+my_list = [4, 6, 2, 8, 3, 1]
+my_list.append(123)
+print(my_list)
+tuple(my_list)
+
+
+my_list = [10, 20, 40, 50, 60, 70, 80, 90, 100]
+print(my_list)
+tuple(my_list)
+
+
+
+my_list = [(), (), ('',), ('a', 'b'), ('a', 'b', 'c'), ('d',)]
+my_list.pop(0)
+my_list.pop(0)
+print(my_list)
+tuple(my_list)
+
+
+
+my_list = [1,2,3]
+
+for i in my_list:
+    print(i)
+tuple(my_list)
+
+
+
+
+my_list = [10,20,40,5,70]
+
+for i in my_list:
+    print(i)
+tuple(my_list)
+
+
+
+
+some_string = -123
+some_string = str(some_string)[1:]
+some_string = some_string[::-1]
+some_string = int('-' + some_string)
+print(some_string)
+
+
+
+
+some_string = '534'
+print(some_string[::-1])
+
+
+
+
+some_string = -12
+some_string = str(some_string)[1:]
+some_string = some_string[::-1]
+some_string = int('-' + some_string)
+print(some_string)
+
+
+
